26c0ebec8ab0511e4ae954315003d7082beaa47d
Albert <u25046021@gmail.com>
2025-11-20 15:10:31 -0300

    Descripción de tus cambios aquí

--- js/appwrite.js
+++ js/appwrite.js
@@ -1,9 +1,10 @@
 // js/appwrite.js
 
-import { Client, Databases } from 'https://cdn.jsdelivr.net/npm/appwrite@10.0.0/dist/web/appwrite.min.js';
+// Usamos el CDN oficial de Appwrite
+import { Client, Databases } from 'https://cdn.appwrite.io/web/sdk/10.0.0/client/web/appwrite.min.js';
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Endpoint público
-    .setProject('691f27f8002fe4890ddd'); // ¡Tu Project ID real!
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('691f27f8002fe4890ddd'); // Tu Project ID real
 
 export const databases = new Databases(client);
